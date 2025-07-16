8966a53795ef1e38b22c9ec6a27a4a766ebb0bef
omonboyweb <xojamurodov404@gmail.com>
2025-07-16 20:52:59 +0500

    this is 4-month exam progect using vanilla js and tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#1e40af", // brend uchun ko‘k
+        accent: "#f59e0b", // aksent uchun sariq
+        searchborder: "#ebf0ff",
+      },
+    },
+  },
+  plugins: [],
+};
